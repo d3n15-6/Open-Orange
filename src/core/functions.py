19e5185352c9355891db53bd419e47a1768b5492
d3n15-6 <112826829+d3n15-6@users.noreply.github.com>
2025-08-31 18:34:32 +0000

    OO - 31 agosto actualizar import para python 3

---
+++ src/core/functions.py
@@ -0,0 +1,12 @@
+import sys
+import pickle
+import builtins
+import types
+from datetime import timedelta, time
+
+from core.Responses import *
+from core.database.Database import Database, DBError, DBConnectionError
+from Log import log
+from BasicFunctions import now, today
+from Query import Query
+from Embeddeb_OpenOrange import *
