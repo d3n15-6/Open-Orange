d04feb32ab270a4b8bc86ae1307bdeb4ef66ab87
d3n15-6 <112826829+d3n15-6@users.noreply.github.com>
2025-09-06 11:33:26 +0000

    OO - 6 septiembre - modernización para la importación dinámica de Py2 a Py3

--- src/core/functions.py
+++ src/core/functions.py
@@ -19,3 +19,14 @@ modules_index = None
 __langdict = None
 
 # Guardamos el import orginal
+__standart_import__ = builtins.__import__
+
+# Hack de importción dinámica (modernizado)
+def __redefined_import__(name, globals=None, locals=None, fromlist=None, level=0):
+    if modules_index and name in modules_index:
+        try:
+            return __standart_import__(modules_index[name][-1], globals, locals, fromlist, level)
+        except Exception:
+            return __standart_import__(name, globals, locals, fromlist, level)
+    return __standart_import__(name, globals, locals, fromlist, level)
+    
